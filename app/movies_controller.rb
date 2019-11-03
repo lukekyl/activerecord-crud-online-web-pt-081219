e0805b325b23b8dbc0cbad83878fcb5049019928
Kyle Luke <th.oldschool@gmail.com>
2019-11-03 15:28:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
   if args = nil
     args = {
-      title: "Home Alone",
-      release_date: 1990
+      :title => "Home Alone",
+      :release_date => 1990
     }
     args
   end
